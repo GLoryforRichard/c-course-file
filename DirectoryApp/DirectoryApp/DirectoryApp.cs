6cb2ddf6780414372e23591a834fa1a94f87874c
RichardAvA <44072670+GLoryforRichard@users.noreply.github.com>
2022-06-16 11:04:12 -0400

    Add files via upload

---
+++ DirectoryApp/DirectoryApp/DirectoryApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;  // Added for File access
+namespace DirectoryApp
+{
+    class DirectoryApp
+    {
+        static void Main(string[] args)
+        {
+            string result;
+            DirectoryInfo Info = new DirectoryInfo(".");
+
+            result = Info.FullName + "\n\n" +
+                    "File Names".PadRight(40) + "Size".PadLeft(20) + "\n";
+            foreach (FileInfo fil in Info.GetFiles("*.*"))
+            {
+                result += fil.Name.PadRight(40) +
+                    fil.Length.ToString("N0").PadLeft(20) + "\n";
+            }
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
